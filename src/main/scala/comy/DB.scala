bd8434ceed35b3c5f19685134dbe5ee7593f8375
Ngoc <ngoc@ngoc-ubuntu.(none)>
2010-06-15 18:52:50 +0900

    Add some doc for DB

--- src/main/scala/comy/DB.scala
+++ src/main/scala/comy/DB.scala
@@ -1,14 +1,24 @@
 package comy
 
 class DB(config: Config) {
+  /**
+   * @return None if there is error (DB is down etc.)
+   */
   def saveUrl(url: String): Option[String] = {
     None
   }
 
+  /**
+   * @return None if there is error (DB is down etc.)
+   */
   def getUrl(key: String): Option[String] = {
     None
   }
 
-  def removeExpiredUrls {
+  /**
+   * @return false if there is error (DB is down etc.)
+   */
+  def removeExpiredUrls: Boolean = {
+    false
   }
 }
