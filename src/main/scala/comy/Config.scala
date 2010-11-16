7e729950224b569fb3ea77b7151e08f0c00cb351
Ngoc <ngoc@ngoc-ubuntu.(none)>
2010-11-16 17:01:53 +0900

    Fix style

--- src/main/scala/comy/Config.scala
+++ src/main/scala/comy/Config.scala
@@ -10,14 +10,13 @@ object Config {
     ret
   }
 
-  val serverPort = properties.getProperty("SERVER_PORT", "8364").toInt
-  val apiIps     = properties.getProperty("API_IPS",     "*").split(",").map(ip => ip.trim)
-  val adminIps   = properties.getProperty("ADMIN_IPS",   "*").split(",").map(ip => ip.trim)
+  val apiIps   = properties.getProperty("allowed_ips.api").split(",").map(ip => ip.trim)
+  val adminIps = properties.getProperty("allowed_ips.admin").split(",").map(ip => ip.trim)
 
-  val dbName               = properties.getProperty("DB_NAME",                 "comy")
-  val dbConnectionsPerHost = properties.getProperty("DB_CONNECTIONS_PER_HOST", "100").toInt
-  val dbExpirationDays     = properties.getProperty("DB_EXPIRATION_DAYS",      "90").toInt
-  val dbAddrs              = properties.getProperty("DB_ADDRS",                "localhost:27017, localhost:27017, localhost:27017").split(",").map(addr => addr.trim)
+  val dbAddrs              = properties.getProperty("db.addrs").split(",").map(addr => addr.trim)
+  val dbConnectionsPerHost = properties.getProperty("db.connections_per_host").toInt
+  val dbName               = properties.getProperty("db.name")
+  val dbExpirationDays     = properties.getProperty("db.expiration_days").toInt
 
   def isApiAllowed(ip: String) = apiIps.exists { ip2 =>
     (ip2 == "*") || (ip2 == ip)
