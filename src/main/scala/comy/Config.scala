c69f5c7e89ddb615dbe0f143c2bf94df631be96f
ゴック <ngoc@gokkus-iMac.local>
2011-03-03 10:38:49 +0900

    Better split

--- src/main/scala/comy/Config.scala
+++ src/main/scala/comy/Config.scala
@@ -6,10 +6,10 @@ import xitrum.{Config => XConfig}
 object Config {
   private val properties = XConfig.loadProperties("comy.properties")
 
-  val apiIps   = properties.getProperty("allowed_ips.api").split(",").map(ip => ip.trim)
-  val adminIps = properties.getProperty("allowed_ips.admin").split(",").map(ip => ip.trim)
+  val apiIps   = properties.getProperty("allowed_ips.api").split(',').map(ip => ip.trim)
+  val adminIps = properties.getProperty("allowed_ips.admin").split(',').map(ip => ip.trim)
 
-  val dbAddrs              = properties.getProperty("db.addrs").split(",").map(addr => addr.trim)
+  val dbAddrs              = properties.getProperty("db.addrs").split(',').map(addr => addr.trim)
   val dbConnectionsPerHost = properties.getProperty("db.connections_per_host").toInt
   val dbName               = properties.getProperty("db.name")
   val dbExpirationDays     = properties.getProperty("db.expiration_days").toInt
