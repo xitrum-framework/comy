2640de754d74ccd6ff570322b323b0ac4e57a9f4
Ngoc Dao <ngoc@ngoc-2.local>
2011-06-17 04:24:27 +0900

    Use dist task from Xitrum plugin

--- project/Build.scala
+++ project/Build.scala
@@ -31,48 +31,7 @@ object MyBuild extends Build {
       libraryDependencies := myLibraryDependencies,
 
       mainClass           := Some("comy.Boot"),
-      distTask,
-      distNeedsPackageBin,  // Must be after distTask
       unmanagedBase in Runtime <<= baseDirectory { base => base / "config" }
     )
   )
-
-  // Task "dist" ---------------------------------------------------------------
-
-  val dist = TaskKey[Unit]("dist", "Prepare target/dist directory, ready for production distribution")
-
-  lazy val distTask = dist <<=
-      (externalDependencyClasspath in Runtime, baseDirectory, target, scalaVersion) map {
-      (libs,                                   baseDir,       target, scalaVersion) =>
-
-    val distDir = target / "dist"
-
-    // Copy bin directory
-    val binDir1 = baseDir / "bin"
-    val binDir2 = distDir / "bin"
-    IO.copyDirectory(binDir1, binDir2)
-    binDir2.listFiles.foreach { _.setExecutable(true) }
-
-    // Copy config directory
-    val configDir1 = baseDir / "config"
-    val configDir2 = distDir / "config"
-    IO.copyDirectory(configDir1, configDir2)
-
-    // Copy public directory
-    val publicDir1 = baseDir / "public"
-    val publicDir2 = distDir / "public"
-    IO.copyDirectory(publicDir1, publicDir2)
-
-    // Copy lib directory
-    val libDir = distDir / "lib"
-
-    // Copy dependencies
-    libs.foreach { lib => IO.copyFile(lib.data, libDir / lib.data.name) }
-
-    // Copy .jar files are created after running "sbt package"
-    val jarDir = new File(target, "scala-" + scalaVersion.replace('-', '.'))
-    (jarDir * "*.jar").get.foreach { file => IO.copyFile(file, libDir / file.name) }
-  }
-
-  val distNeedsPackageBin = dist <<= dist.dependsOn(packageBin in Compile)
 }
