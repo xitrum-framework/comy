2c23f60a7f37a3e5a6af247b84ddb64e3a49a513
Ngoc Dao <ngocdaothanh@gmail.com>
2013-04-11 17:21:47 +0900

    Fix views for Xitrum 2

--- src/main/scala/comy/action/Users.scala
+++ src/main/scala/comy/action/Users.scala
@@ -28,38 +28,39 @@ class UserQRcode extends AppAction {
 @POST("user/shorten")
 class UserShorten extends AppAction {
   def execute() {
-    val url = param("url").trim
-    if (url.isEmpty) {
+    val toBeShorten = param("url").trim
+    if (toBeShorten.isEmpty) {
       jsRespond("$('#result').html('%s')".format(jsEscape(<p class="error">{t("URL must not be empty")}</p>)))
-    } else {
-      val keyo = {
-        val ret = param("key").trim
-        if (ret.isEmpty) None else Some(ret)
-      }
-      val (resultCode, resultString) = DB.saveUrl(this, url, keyo)
-
-      // This causes error with Scala 2.10.0:
-      //val html = resultCode match {...
-
-      val html: scala.xml.Node = resultCode match {
-        case SaveUrlResult.VALID =>
-          val absoluteUrl = absUrl[ApiLengthen]("key" -> resultString)
-          <xml:group>
-            <hr />
-            {absoluteUrl}<br />
-            <a href={absoluteUrl} target="_blank"><img src={url[UserQRcode]("url" -> absoluteUrl)} /></a>
-          </xml:group>
-
-        case SaveUrlResult.INVALID =>
-          <p class="error">{resultString}</p>
-
-        case SaveUrlResult.DUPLICATE =>
-          <p class="error">{t("Key has been chosen")}</p>
-
-        case SaveUrlResult.ERROR =>
-          <p class="error">{t("Server error")}</p>
-      }
-      jsRespond("$('#result').html('%s')".format(jsEscape(html)))
+      return
     }
+
+    val keyo = {
+      val ret = param("key").trim
+      if (ret.isEmpty) None else Some(ret)
+    }
+    val (resultCode, resultString) = DB.saveUrl(this, toBeShorten, keyo)
+
+    // This causes error with Scala 2.10.0:
+    //val html = resultCode match {...
+
+    val html: scala.xml.Node = resultCode match {
+      case SaveUrlResult.VALID =>
+        val absoluteUrl = absUrl[ApiLengthen]("key" -> resultString)
+        <xml:group>
+          <hr />
+          {absoluteUrl}<br />
+          <a href={absoluteUrl} target="_blank"><img src={url[UserQRcode]("url" -> absoluteUrl)} /></a>
+        </xml:group>
+
+      case SaveUrlResult.INVALID =>
+        <p class="error">{resultString}</p>
+
+      case SaveUrlResult.DUPLICATE =>
+        <p class="error">{t("Key has been chosen")}</p>
+
+      case SaveUrlResult.ERROR =>
+        <p class="error">{t("Server error")}</p>
+    }
+    jsRespond("$('#result').html('%s')".format(jsEscape(html)))
   }
 }
